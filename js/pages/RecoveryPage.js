361baa642365558a61e15af3f8771bbb890bace9
bensonkm <bensonkm@bc.edu>
2025-11-19 22:57:47 -0500

    Update RecoveryPage.js
    Added another section

--- js/pages/RecoveryPage.js
+++ js/pages/RecoveryPage.js
@@ -32,9 +32,16 @@
                 Learn more about on-campus resources for students struggling with mental illnesses through <a href="https://www.bc.edu/bc-web/offices/studentaffairs/sites/health-wellness/counseling/services.html#consultations" target="_blank" rel="noopener">Boston College University Counseling</a>.
               </p>
             </div>
+            <div class= "resource-item">
+            <h3>Other Options:</h3>
+                <p>
+                    Learn more about resources outside of Boston College through <a href="https://www.mass.gov/guides/finding-mental-health-support-in-massachusetts">Massachussetts mental health support services</a>.
+                </p>
+            </div>
           </section>
         </main>
       `;
     }
   };
 })();
+
